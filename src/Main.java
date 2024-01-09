1b2389a9e8cb73edbc88bbd498f8096904140d2b
mina793878 <tnals793878@gmail.com>
2024-01-09 15:52:06 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//import java.util.*;
+public class Main {
+
+	public static void main(String [] args) {
+	
+		Human human = new Human("Mina",24,55);
+		Human human2 = new Human("Minghao",27,58);
+		
+		
+		human2.eat();
+		human.drink();
+		//System.out.println(human2.name);
+	}
+}
